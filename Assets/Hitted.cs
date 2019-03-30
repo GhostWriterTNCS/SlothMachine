d8f481e5b7dade56f24155ad9adb6df98debf712
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-03-30 10:32:02 +0100

    Initial commit.

---
+++ Assets/Hitted.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitted : MonoBehaviour
+{
+	public Animator animator;
+	List<Collider> siblings = new List<Collider>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		Transform t = transform.parent;
+		while(t.parent) {
+			t = t.parent;
+		}
+		siblings.AddRange(t.GetComponentsInChildren<Collider>());
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (animator && !siblings.Contains(other) && other.isTrigger) {
+			Debug.Log(name + " collided by " + other.name);
+			other.enabled = false;
+			animator.SetTrigger("Reaction");
+			//animator.Play("Reaction", -1, 0);
+		}
+	}
+}
---
+++ Assets/MyAnimator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Animator))]
+public class MyAnimator : NetworkBehaviour {
+	Animator animator;
+	public Camera camera;
+	public float moveSpeed = 3;
+	public Collider leftHand;
+	public Collider rightHand;
+
+	void Start() {
+		if (isLocalPlayer) {
+			animator = GetComponent<Animator>();
+			leftHand.enabled = false;
+			rightHand.enabled = false;
+		} else {
+			camera.enabled = false;
+			camera.GetComponent<AudioListener>().enabled = false;
+		}
+	}
+
+	void Update() {
+		if (isLocalPlayer) {
+			transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed, transform.position.y, transform.position.z + Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+			animator.SetFloat("WalkH", Input.GetAxis("Horizontal"));
+			animator.SetFloat("WalkV", Input.GetAxis("Vertical"));
+
+			AnimationClip animationClip = new AnimationClip();
+			if (animator.GetCurrentAnimatorClipInfo(0).Length > 0) {
+				animationClip = animator.GetCurrentAnimatorClipInfo(0)[0].clip;
+			}
+			if (Input.GetButtonDown("X")) {
+				animator.SetTrigger("Punch");
+			} else if (Input.GetButtonDown("Y")) {
+				animator.SetTrigger("Boxing");
+			}
+		}
+	}
+}
