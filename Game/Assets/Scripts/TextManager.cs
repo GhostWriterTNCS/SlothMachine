99adf171a7271bbd4f3056724a8c055cc5ba3221
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-04-26 15:46:30 +0200

    Improvements.

--- Game/Assets/Scripts/TextManager.cs
+++ Game/Assets/Scripts/TextManager.cs
@@ -1,29 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
+[ExecuteInEditMode]
 public class TextManager : MonoBehaviour {
+	public static string fontName = "NovaMono";
 	public static int fontSize;
 	public static int fontSizeH;
+	public static Color fontColor = new Color(0.8654326f, 0.9438342f, 0.9811321f);
 
-	// Start is called before the first frame update
 	void Awake() {
-		if (fontSize == 0) {
-			fontSize = Screen.height / 43;
-		}
-		if (fontSizeH == 0) {
-			fontSizeH = Screen.height / 26;
-		}
+		fontSize = Screen.height / 31;
+		fontSizeH = Screen.height / 16;
 		foreach (Text t in FindObjectsOfType<Text>()) {
-			if (t.GetComponent<TextHeader>()) {
-				t.fontSize = fontSizeH;
+			TextProperties tp = t.GetComponent<TextProperties>();
+			if (tp) {
+				if (tp.type == TextProperties.TextType.Header) {
+					t.fontSize = fontSizeH;
+					t.resizeTextForBestFit = false;
+				} else if (tp.type == TextProperties.TextType.Normal) {
+					t.fontSize = fontSize;
+					t.resizeTextForBestFit = false;
+				} else if (tp.type == TextProperties.TextType.Expand) {
+					t.resizeTextForBestFit = true;
+					t.resizeTextMaxSize = 300;
+				}
 			} else {
 				t.fontSize = fontSize;
 			}
+			if (!tp || !tp.useCustomColor) {
+				t.color = fontColor;
+			}
+			t.font = (Font)Resources.Load(fontName);
 		}
-	}
-
-	// Update is called once per frame
-	void Update() {
-
 	}
 }
---
+++ Game/Assets/Scripts/TextProperties.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TextProperties : MonoBehaviour {
+	public enum TextType {
+		Normal,
+		Header,
+		Expand
+	}
+	public TextType type;
+	public bool useCustomColor;
+}
