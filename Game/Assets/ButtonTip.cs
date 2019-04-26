19d223e29f526e9ee1c25c8f64b64fa64cc48b57
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-04-26 19:17:52 +0200

    Improvements.

---
+++ Game/Assets/ButtonTip.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Image))]
+public class ButtonTip : MonoBehaviour {
+	public string button;
+	Image image;
+
+	void Awake() {
+		Refresh();
+	}
+
+	public void Refresh() {
+		if (!image) {
+			image = GetComponent<Image>();
+		}
+		image.sprite = Resources.Load<Sprite>("Controls\\" + InputDetector.controller + "\\" + button);
+		image.preserveAspect = true;
+	}
+}
---
+++ Game/Assets/Scripts/InputDetector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class InputDetector : MonoBehaviour {
+	public static string controller = "Xbox One";
+	void Update() {
+		if (Input.GetJoystickNames().Length > 0) {
+			if (Input.GetJoystickNames()[0].ToLower().Contains("xbox 360") && controller != "Xbox 360") {
+				controller = "Xbox 360";
+				foreach (ButtonTip bt in FindObjectsOfType<ButtonTip>()) {
+					bt.Refresh();
+				}
+			} else if (Input.GetJoystickNames()[0].ToLower().Contains("ps4") && controller != "PS4") {
+				controller = "PS4";
+				foreach (ButtonTip bt in FindObjectsOfType<ButtonTip>()) {
+					bt.Refresh();
+				}
+			}
+		}
+	}
+}
