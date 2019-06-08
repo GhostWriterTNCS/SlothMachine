eca37b16e601dd67ac2e1abb27d53af0a4da78ad
Francesco Dettori <francesco030996@gmail.com>
2019-06-08 12:04:54 +0200

    Aggiunto modello pinguino e vari script bisogna vedere se funziona

---
+++ Game/Assets/Scripts/Arena/Penguis' Flocking/PenguinsBlending.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinsBlending : MonoBehaviour
+{
+    private Collider[] neighbors = new Collider[200];
+    // this must be large enough
+
+    void FixedUpdate()
+    {
+
+        Vector3 globalDirection = Vector3.zero;
+
+        int count = Physics.OverlapSphereNonAlloc(transform.position, PenguinsShared.PenguinFOW, neighbors);
+
+        foreach (PenguinsComponent bc in GetComponents<PenguinsComponent>())
+        {
+            globalDirection += bc.GetDirection(neighbors, count);
+        }
+
+        if (globalDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation((globalDirection.normalized + transform.forward) / 2f);
+        }
+
+        transform.position += transform.forward * PenguinsShared.PenguinSpeed * Time.deltaTime;
+    }
+}
---
+++ Game/Assets/Scripts/Arena/Penguis' Flocking/PenguinsComponent.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class PenguinsComponent : MonoBehaviour
+{
+    public abstract Vector3 GetDirection(Collider[] neighbors, int size);
+}
+
