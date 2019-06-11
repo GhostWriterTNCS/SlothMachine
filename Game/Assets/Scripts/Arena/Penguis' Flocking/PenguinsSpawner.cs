f2fe44afbe6564118f7ab8a131b1ad4214ca83b1
Francesco Dettori <francesco030996@gmail.com>
2019-06-11 09:37:53 +0200

    Aggiustato correttamente spwan pinguini e settaggio per prova

--- Game/Assets/Scripts/Arena/Penguis' Flocking/PenguinsSpawner.cs
+++ Game/Assets/Scripts/Arena/Penguis' Flocking/PenguinsSpawner.cs
@@ -8,18 +8,19 @@ public class PenguinsSpawner : MonoBehaviour
     public int count = 30;
     public GameObject penguin = null;
 
-    void Start()
+
+    public void spwanPenguins()
     {
         if (penguin != null)
         {
             for (int i = 0; i < count; i += 1)
             {
-                GameObject go = Instantiate(penguin, transform.position + Random.insideUnitSphere * radius, Quaternion.EulerAngles(0,Random.RandomRange(0,359),0));
+                GameObject go = Instantiate(penguin, transform.position + Random.insideUnitSphere * radius, Quaternion.EulerAngles(0, Random.RandomRange(0, 359), 0));
                 go.transform.LookAt(transform.position + Random.insideUnitSphere * radius);
-                go.GetComponentInChildren<MeshRenderer>().transform.Rotate(90,0,0);
-                go.transform.position= new Vector3(go.transform.position.x, 0.8f, go.transform.position.z);
+                go.GetComponentInChildren<MeshRenderer>().transform.Rotate(90, 0, 0);
+                go.transform.position = new Vector3(go.transform.position.x, 0.8f, go.transform.position.z);
                 go.name = penguin.name + " " + i;
-               
+
             }
         }
     }
