e0cc362c1fc32db8040f033f8908fa94a4b361b7
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-06-13 14:53:34 +0200

    Added F6 to skip to boss arena. Fixed animations not hitting. Enlarged hitboxes.

---
+++ Game/Assets/Scripts/Arena/SkipToBossArena.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SkipToBossArena : MonoBehaviour {
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.F6)) {
+			int scoreMax = -1;
+			Player roundWinner = null;
+			foreach (Player p in FindObjectsOfType<Player>()) {
+				p.robot.paused = true;
+				if (p.robot.roundScore > scoreMax) {
+					scoreMax = p.robot.roundScore;
+					roundWinner = p;
+				}
+			}
+			roundWinner.roundWinner = 2;
+			MatchManager.singleton.bossRound = true;
+			NetworkManager.singleton.ServerChangeScene(GameScenes.Arena);
+		}
+	}
+}
