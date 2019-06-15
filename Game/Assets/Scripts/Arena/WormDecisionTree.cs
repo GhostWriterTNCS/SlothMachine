ae343b29ebca339b1ae057342de44f392f2b8caa
Francesco Dettori <francesco030996@gmail.com>
2019-06-15 22:30:40 +0200

    Modificato decision tree del worm

--- Game/Assets/Scripts/Arena/WormDecisionTree.cs
+++ Game/Assets/Scripts/Arena/WormDecisionTree.cs
@@ -26,17 +26,19 @@ public class WormDecisionTree : NetworkBehaviour {
 		// Define actions
 		DTAction a1 = new DTAction(moveToBoundaries);
 		DTAction a2 = new DTAction(attackRandomly);
+        DTAction a3 = new DTAction(waitFreezen);
 
-		// Define decisions
-		DTDecision d1 = new DTDecision(checkLastAttack);
+        // Define decisions
+        DTDecision d1 = new DTDecision(checkLastAttack);
 		DTDecision d2 = new DTDecision(checkRobotAround);
 
 
 		// Link action with decisions
 		d1.AddLink(false, d2);
-		d1.AddLink(true, a1);
+		d1.AddLink(true, a3);
 
 		d2.AddLink(true, a2);
+        d2.AddLink(false, a1);
 
 		// Setup my DecisionTree at the root node
 		dt = new DecisionTree(d1);
@@ -57,6 +59,7 @@ public class WormDecisionTree : NetworkBehaviour {
 	}
 
 	Transform destination;
+    Vector3 randomPosition;
 	private void Update() {
 		if (destination) {
 			Vector3 verticalAdj = new Vector3(destination.position.x, transform.position.y, destination.position.z);
@@ -66,7 +69,25 @@ public class WormDecisionTree : NetworkBehaviour {
 			transform.LookAt(verticalAdj);
 			Rigidbody rb = GetComponent<Rigidbody>();
 			myRigidbody.MovePosition(myRigidbody.position + transform.forward * moveSpeed * Time.deltaTime);
-		}
+		}else if (randomPosition!=Vector3.zero)
+        {
+            if((randomPosition - transform.position).magnitude >= 2f)
+            {
+                Vector3 toDestination = (randomPosition - transform.position);
+                transform.LookAt(randomPosition);
+                Rigidbody rb = GetComponent<Rigidbody>();
+                myRigidbody.MovePosition(myRigidbody.position + transform.forward * moveSpeed * Time.deltaTime);
+            }
+            else
+            {
+                //Debug.Log("Verme vicino alla posizione");
+                randomPosition = Vector3.zero;
+                //Debug.Log("nuova posizione settata a zero"+ randomPosition);
+                timer = waitDuration;
+                StartCoroutine(RespawnCoroutine());
+            }
+            
+        }
 	}
 
 	// Take decision every interval, run forever
@@ -90,30 +111,46 @@ public class WormDecisionTree : NetworkBehaviour {
 
 	//check the nearest player in the range
 	public object checkRobotAround(object o) {
-		if (playerTarget) {
-			return true;
-		}
-		//Debug.Log("checkRobotAround");
-		GetComponent<Collider>().enabled = true;
-		particle.Play();
-		System.Random rnd = new System.Random();
-		Robot[] arr = FindObjectsOfType<Robot>().OrderBy(x => rnd.Next()).ToArray();
-		foreach (Robot robot in arr) {
-			if ((robot.transform.position - transform.position).magnitude <= range) {
-				playerTarget = robot;
-				return true;
-			}
-		}
+        timerArena = FindObjectOfType<NetworkArenaManager>().roundDuration;
+        
+        if(timerArena <= spawnTime)
+        {
+            if (playerTarget)
+            {
+                return true;
+            }
+            //Debug.Log("checkRobotAround");
+            GetComponent<Collider>().enabled = true;
+            particle.Play();
+            System.Random rnd = new System.Random();
+            Robot[] arr = FindObjectsOfType<Robot>().OrderBy(x => rnd.Next()).ToArray();
+            foreach (Robot robot in arr)
+            {
+                if ((robot.transform.position - transform.position).magnitude <= range)
+                {
+                    playerTarget = robot;
+                    return true;
+                }
+            }
 
-		//attack the player who has the highest score
-		int scoreMax = -1;
-		foreach (Robot robot in arr) {
-			if (robot.player && robot.player.score > scoreMax) {
-				scoreMax = robot.player.score;
-				playerTarget = robot;
-			}
-		}
-		return true;
+            //attack the player who has the highest score
+            int scoreMax = -1;
+            foreach (Robot robot in arr)
+            {
+                if (robot.player && robot.player.score > scoreMax)
+                {
+                    scoreMax = robot.player.score;
+                    playerTarget = robot;
+                }
+            }
+            return true;
+        }
+        else
+        {
+            //Debug.Log("Sono in checkRobotAround e ritorno false perche non ancora meta tempo");
+            return false;
+        }
+		
 	}
 
 
@@ -128,12 +165,31 @@ public class WormDecisionTree : NetworkBehaviour {
 		return null;
 	}
 
+    public object waitFreezen(object o)
+    {
+        
+        return null;
+    }
 
-	//move randomly for an amount of time near the boundaries of the map
-	public object moveToBoundaries(object o) {
-		//GetComponent<Collider>().enabled = false;
-		//particle.Stop();
-		return null;
+
+    //move randomly for an amount of time near the boundaries of the map
+    public object moveToBoundaries(object o) {
+        //GetComponent<Collider>().enabled = false;
+        //particle.Stop();
+        if (randomPosition != Vector3.zero)
+        {
+            //Debug.Log("MoveToBoundaries la condizione randomPosition != Vector3.zero "+ (randomPosition != Vector3.zero));
+            return true;
+        }
+        GetComponent<Collider>().enabled = true;
+        particle.Play();
+        float x = Random.Range(225f, 310f);
+        float z = Random.Range(190f, 275f);
+        float y = FindObjectOfType<Terrain>().terrainData.GetHeight((int)x, (int)z);
+
+        randomPosition = new Vector3(x, y, z);
+        //Debug.Log("Posizione del verme " + randomPosition);
+        return null;
 	}
 
 
@@ -145,7 +201,7 @@ public class WormDecisionTree : NetworkBehaviour {
 				Debug.Log("timerArena < " + spawnTime);
 				FindObjectOfType<NetworkArenaManager>().CmdSpawnWorm(wormPrefab, new Vector3(transform.position.x, transform.position.y - 10, transform.position.z));
 			} else {
-				other.GetComponent<Rigidbody>().AddForce(transform.forward * 12.5f, ForceMode.Impulse);
+				other.GetComponent<Rigidbody>().AddForce(transform.forward * 9.5f, ForceMode.Impulse); //forza era a 12.5
 			}
 			Debug.Log("colpito");
 
