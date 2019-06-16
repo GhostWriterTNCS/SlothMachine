62d387125ddaf92518edebb9ca4b5dded6b9b915
Francesco Dettori <francesco030996@gmail.com>
2019-06-16 10:26:32 +0200

    Scena per trailer montata

---
+++ Game/Assets/Scripts/Arena/PerlinNoisePerSceneTrailer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Random = UnityEngine.Random;
+
+public class PerlinNoisePerSceneTrailer : MonoBehaviour {
+
+	[Serializable]
+	public class Count {
+		public int maximum;
+		public int minimum;
+
+		public Count(int min, int max) {
+			maximum = max;
+			minimum = min;
+
+		}
+	}
+
+	public int dept = 90;
+	public int width = 513;
+	public int height = 513;
+	public float scaleMin = 1.6f;
+	public float scaleMax = 3.2f;
+
+	public Count cactusCount = new Count(160, 350);
+	public Count rockCount = new Count(50, 130);
+	public GameObject[] cactusTiles = new GameObject[3];
+	public GameObject[] rockTiles = new GameObject[2];
+	public float scale;
+	private List<Vector3> gridPositions = new List<Vector3>(); //for position in the map
+	private Transform boardHolder;
+	public GameObject ArenaSphere;
+
+	//[SyncVar]
+	public int randomSeed = 0;
+
+	public void Start() {
+		Random.seed = randomSeed;
+		scale = Random.Range(scaleMin, scaleMax);
+
+		Terrain terrain = GetComponent<Terrain>();
+		terrain.terrainData = GenerateTerrain(terrain.terrainData);
+		/*cactusTiles[0] = Resources.Load("Prefabs/Arena/Cactus01") as GameObject;
+		cactusTiles[1] = Resources.Load("Prefabs/Arena/Cactus02") as GameObject;
+		cactusTiles[2] = Resources.Load("Prefabs/Arena/Plant01") as GameObject;
+		rockTiles[0] = Resources.Load("Prefabs/Arena/Rock01") as GameObject;
+		rockTiles[1] = Resources.Load("Prefabs/Arena/Bone") as GameObject;
+		ArenaSphere = Resources.Load("Prefabs/ArenaSphere") as GameObject;*/
+		SpawnObject(terrain.terrainData);
+
+		//spawnArenaSphere();
+
+		/*foreach (NetworkStartPosition pos in FindObjectsOfType<NetworkStartPosition>()) {
+			Vector3 v3 = pos.transform.position;
+			pos.transform.position = new Vector3(v3.x, terrain.terrainData.GetHeight((int)v3.x, (int)v3.z) + 3.5f, v3.z);
+		}*/
+		//FindObjectOfType<ArenaManager>().arenaReady = true;
+		//CmdFixWormPosition();
+
+	}
+
+	//[Command]
+	public void CmdFixWormPosition() {
+		StartCoroutine(FindObjectOfType<WormDecisionTree>().RespawnCoroutine());
+	}
+
+	TerrainData GenerateTerrain(TerrainData terrainData) {
+		terrainData.heightmapResolution = width + 1;
+		terrainData.size = new Vector3(width, dept, height);
+
+		terrainData.SetHeights(0, 0, GenerateHeights());
+		return terrainData;
+	}
+
+	float[,] GenerateHeights() {
+		float[,] heights = new float[width, height];
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				heights[x, y] = CalculateHeight(x, y);
+			}
+		}
+		return heights;
+	}
+
+	float CalculateHeight(int x, int y) {
+		float xCoord = (float)x / width * scale;
+		float yCoord = (float)y / height * scale;
+		return Mathf.PerlinNoise(xCoord, yCoord);
+	}
+
+	public void SpawnObject(TerrainData terrainData) {
+		gridPositions.Clear();
+		for (int x = 0; x < width - 1; x++) {
+			//Within each column, loop through y axis (rows).
+			for (int y = 1; y < height - 1; y++) {
+				//At each index add a new Vector3 to our list with the x, y, z coordinates of that position.
+				gridPositions.Add(new Vector3(x, terrainData.GetHeight(x, y), y));
+			}
+		}
+
+		LayoutObjectAtRandom(cactusTiles, cactusCount.minimum, cactusCount.maximum);
+		LayoutObjectAtRandom(rockTiles, rockCount.minimum, rockCount.maximum);
+	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int min, int max) {
+		int objectCount = Random.Range(min, max + 1);
+
+		for (int i = 0; i < objectCount; i++) {
+			Vector3 randomPosition = RandomPosition();
+			GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+			Instantiate(tileChoice, randomPosition, Quaternion.identity, transform);
+
+		}
+	}
+
+	Vector3 RandomPosition() {
+		int randomIndex = Random.Range(0, gridPositions.Count);
+		Vector3 randomPosition = gridPositions[randomIndex];
+		gridPositions.RemoveAt(randomIndex);
+		return randomPosition;
+	}
+
+
+	public void spawnArenaSphere() {
+		Instantiate(ArenaSphere, new Vector3(width / 2, GetComponent<Terrain>().terrainData.GetHeight(width / 2, height / 2), height / 2), Quaternion.identity);
+	}
+
+}
---
+++ Game/Assets/Scripts/Arena/PlayerMovePerTrailer.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovePerTrailer : MonoBehaviour {
+	public float moveSpeed = 3;
+	public float moveSpeedMultiplier = 1;
+	public float moveSpeedAdjust = 3;
+	public float turnSpeed = 3;
+	public bool canMove = true;
+	public bool canRotateCamera = true;
+	public bool isAttacking = false;
+	public float walkH = 0;
+	public float walkV = 0;
+
+	Rigidbody rigidbody;
+	//Animator animator;
+	Robot robot;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody>();
+		//animator = GetComponent<Animator>();
+		robot = GetComponent<Robot>();
+	}
+
+	void Update() {
+		if (canMove) {
+			// Move player
+			//if (!isAttacking) {
+			float adjustSpeed = 1;
+            /*if (robot.lockCameraRobot) {
+				float dist = Vector3.Distance(transform.position, robot.lockCameraRobot.transform.position) / moveSpeedAdjust;
+				if (dist < 1) {
+					adjustSpeed = dist;
+				}
+			}*/
+            if (Input.GetKey("w"))
+            {
+                GetComponent<Animator>().SetFloat("WalkV", 1);
+                rigidbody.MovePosition(rigidbody.position + (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")) * Time.deltaTime * moveSpeed * moveSpeedMultiplier * adjustSpeed);
+            }
+            else
+            {
+                GetComponent<Animator>().SetFloat("WalkV", 0);
+                if (isAttacking)
+                {
+                    adjustSpeed *= 0.2f;
+                }
+                rigidbody.MovePosition(rigidbody.position + (transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal")) * Time.deltaTime * moveSpeed * moveSpeedMultiplier * adjustSpeed);
+                if (isAttacking)
+                {
+                    walkH = 0;
+                    walkV = 0;
+                    //robot.SetFloat("WalkH", 0);
+                    //robot.SetFloat("WalkV", 0);
+                }
+                else
+                {
+                    walkH = Input.GetAxis("Horizontal");
+                    walkV = Input.GetAxis("Vertical");
+                    //robot.SetFloat("WalkH", Input.GetAxis("Horizontal"));
+                    //robot.SetFloat("WalkV", Input.GetAxis("Vertical"));
+                }
+                /*} else {
+                    animator.SetFloat("WalkH", 0);
+                    animator.SetFloat("WalkV", 0);
+                }*/
+                // Rotate camera
+                if (canRotateCamera)
+                {
+                    rigidbody.MoveRotation(rigidbody.rotation * Quaternion.Euler(Vector3.up * Input.GetAxis("Camera Horizontal") * turnSpeed));
+                }
+            }
+
+			
+		} else {
+			walkH = 0;
+			walkV = 0;
+			//robot.SetFloat("WalkH", 0);
+			//robot.SetFloat("WalkV", 0);
+		}
+	}
+}
