6a5e5d61d59b3c6ad905553fe214a9947c6938a2
GhostWriterTNCS <ghostwriter.tncs@gmail.com>
2019-06-16 15:15:15 +0200

    Fixed penguins' push.

---
+++ Game/Assets/Scripts/Arena/Penguis' Flocking/PenguinPush.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PenguinPush : MonoBehaviour {
+	public float pushForce;
+
+	public void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<Robot>()) {
+			other.GetComponent<Rigidbody>().AddForce((transform.forward + (transform.up * 0.5f)) * pushForce, ForceMode.Impulse);
+		}
+	}
+}
