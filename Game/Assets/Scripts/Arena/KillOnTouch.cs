e72bab1d4651e11f06a6c27329ec6579ea8363a1
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-06-18 19:54:29 +0200

    Cleanup. Added arenaReady condition for KillOnTouch.

--- Game/Assets/Scripts/Arena/KillOnTouch.cs
+++ Game/Assets/Scripts/Arena/KillOnTouch.cs
@@ -2,9 +2,11 @@
 
 public class KillOnTouch : MonoBehaviour {
 	private void OnTriggerEnter(Collider other) {
-		Robot p = other.GetComponent<Robot>();
-		if (p && p.player) {
-			p.UpdateHealth(-p.healthMax);
+		if (FindObjectOfType<ArenaManager>().arenaReady) {
+			Robot p = other.GetComponent<Robot>();
+			if (p && p.player) {
+				p.UpdateHealth(-p.healthMax);
+			}
 		}
 	}
 }
---
+++ Game/Assets/Scripts/Arena/Worm/WormDestroyer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WormDestroyer : MonoBehaviour {
+	public void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<WormHitbox>()) {
+			Destroy(other.gameObject);
+		}
+	}
+}
