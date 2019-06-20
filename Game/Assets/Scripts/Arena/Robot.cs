08002026d8cf0d7638df692d6d086c52b1aee600
Ghost Writer TNCS <ghostwriter.tncs@gmail.com>
2019-06-20 14:25:01 +0200

    Bugfix and improvements for SyncTransform. Added Kill on touch collider under desert.

--- Game/Assets/Scripts/Arena/Robot.cs
+++ Game/Assets/Scripts/Arena/Robot.cs
@@ -224,26 +224,22 @@ public class Robot : NetworkBehaviour {
 		} else {
 			//CmdSpawn();
 			int bossID = 0;
-			foreach (Player p in FindObjectsOfType<Player>()) {
-				if (p.roundWinner >= 2) {
-					bossID = p.playerID;
-					break;
+			if (MatchManager.singleton.bossRound) {
+				foreach (Player p in FindObjectsOfType<Player>()) {
+					if (p.roundWinner >= 2) {
+						bossID = p.playerID;
+						break;
+					}
 				}
 			}
-			Transform spawn;
-			if (player.playerID > bossID) {
+			Transform spawn = FindObjectsOfType<SpawnPoint>()[player.playerID - 1].transform;
+			if (bossID > 0 && player.playerID > bossID) {
 				spawn = FindObjectsOfType<SpawnPoint>()[player.playerID - 2].transform;
-			} else {
-				spawn = FindObjectsOfType<SpawnPoint>()[player.playerID - 1].transform;
 			}
 			transform.position = spawn.position;
 			transform.rotation = spawn.rotation;
 			rigidbody.velocity = Vector3.zero;
 		}
-		//Debug.Log("Spawn " + player.name + " in " + spawn.position);
-		/*transform.position = spawn.position;
-		transform.rotation = spawn.rotation;
-		rigidbody.velocity = Vector3.zero;*/
 		body.enabled = true;
 
 		if (isLocalPlayer) {
--- Game/Assets/Scripts/SyncTransform.cs
+++ Game/Assets/Scripts/SyncTransform.cs
@@ -7,6 +7,7 @@ using UnityEngine.Networking;
 public class SyncTransform : NetworkBehaviour {
 	public float transitionSpeed = 5;
 	public float snapThreshold = 15;
+	public float sendRate = 0.1f;
 	[SyncVar]
 	public Vector3 position;
 	[SyncVar]
@@ -14,30 +15,44 @@ public class SyncTransform : NetworkBehaviour {
 	/*[SyncVar]
 	public bool respawn;*/
 
+	//[SyncVar]
 	bool cmdEnabled = false;
 	PlayerMove playerMove;
 
 	void Start() {
 		if (isLocalPlayer) {
-			cmdEnabled = true;
+			CmdEnable(true);
 		}
 		playerMove = GetComponent<PlayerMove>();
 	}
 
-	[Command]
+	//[Command]
 	public void CmdEnable(bool value) {
 		cmdEnabled = value;
+		if (cmdEnabled) {
+			StartCoroutine(SetValuesCoroutine());
+		}
+	}
+
+	IEnumerator SetValuesCoroutine() {
+		while (true) {
+			if (cmdEnabled/*&& !respawn*/) {
+				//Debug.Log("My position is " + transform.position);
+				CmdSetValues(transform.position, transform.rotation);
+				yield return new WaitForSeconds(sendRate);
+			} else {
+				yield break;
+			}
+		}
 	}
 
 	void Update() {
-		if (cmdEnabled /*&& !respawn*/) {
-			//Debug.Log("My position is " + transform.position);
-			CmdSetValues(transform.position, transform.rotation);
-		} else {
-			if (Vector3.Distance(transform.position, position) > snapThreshold) {
+		if (!cmdEnabled) {
+			float dist = Vector3.Distance(transform.position, position);
+			if (dist > snapThreshold) {
 				transform.position = position;
 			} else {
-				transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * playerMove.moveSpeed);
+				transform.position = Vector3.Lerp(transform.position, position, Time.deltaTime * playerMove.moveSpeed * (dist < 0.5f ? dist * 2 : 1));
 			}
 			transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * playerMove.moveSpeed);
 			//respawn = false;
